2e05a927cf919b9eb30eaf559b9160eaaf09532b
Niklas.Budimir <4124@htl.rennweg.at>
2017-11-12 13:04:33 +0100

    new files

---
+++ src/Album.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace nb;
+
+class Album{
+
+    private $interpret;
+    private $title;
+    private $label;
+
+    public function __construct(string $interpret, string $title, string $label)
+    {
+       $this->interpret=$interpret;
+       $this->title=$title;
+       $this->label=$label;
+    }
+
+    public function __toString()
+    {
+        return "Titel: ".$this->title." | Interpret: ".$this->interpret." | Label: ".$this->label;
+    }
+
+}
